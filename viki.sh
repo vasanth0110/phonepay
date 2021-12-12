78d333b3d4ac0a28066641d5428c70e0cde1f4a7
vasanth0110 <vasanthvsv1619@gmail.com>
2021-12-12 15:15:08 +0530

    created

---
+++ viki.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+USER='username'
+COMMIT_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S %Z'`
+DATELOG=`date +'%Y-%m-%d-%H-%M-%S'`
+LOG="/tmp/${DATELOG}.txt"
+
+# Only proceed if we have a valid repo.
+if [ ! -d ${REPO}/.git ]; then
+  echo "${REPO} is not a valid git repo! Aborting..." >> ${LOG}
+  exit 0
+else
+  echo "${REPO} is a valid git repo! Proceeding..." >> ${LOG}
+fi
+
+cd ${REPO}
+git log --pretty=format:"%h %ad | %s %d [%an]" --date=short
+${GIT} add --all . >> ${LOG}
+${GIT} commit -m "Automated commit on ${COMMIT_TIMESTAMP}" >> ${LOG}
+${GIT} push git@bitbucket.org:username/repo.git master >> ${LOG}
+
+# Depends on libnotify
+${NOTIFY} 'KB notification' 'Changes were pushed to Bitbucket.' --icon=dialog-information >> ${LOG}
+
